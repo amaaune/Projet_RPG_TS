58eba9ada514987c4385e06d6f4a70316fe7d70f
aamaury <amaury.aune@ynov.com>
2026-01-27 10:56:54 +0100

    modified monster based on SUPErclass

--- monsters/Monsters.ts
+++ monsters/Monsters.ts
@@ -1,41 +1,6 @@
 import { Character } from "../src/Characters.ts";
 
 export class Monster {
-    name : string;
-    attackP : number;
-    deffP : number;
-    attackM : number;
-    deffM : number;
-    speed : number;
-    maxHp : number;
-    currentHp : number;
-
-     constructor(
-    name: string,
-    attackP: number,
-    deffP: number,
-    attackM: number,
-    deffM: number,
-    speed: number,
-    maxHp: number,
-  ) {
-    this.name = name;
-    this.attackP = attackP;
-    this.deffP = deffP;
-    this.attackM = attackM;
-    this.deffM = deffM;
-    this.speed = speed;
-    this.maxHp = maxHp;
-    this.currentHp = maxHp;
-  }
-  /** Vérifie si le personnage est vivant */
-  isAlive() : boolean {
-    return this.currentHp > 0;
-  }
-
-//   aoe(target : Character[]) : number {
-
-//   }
 
   random(targets : Character[]) {
     const aliveTarget = targets.filter(t => t.isAlive());
